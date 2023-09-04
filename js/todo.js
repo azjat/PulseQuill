1605041cfe8be3e8d493f2c98dac6ce0717e2645
azjat <90611765+azjat@users.noreply.github.com>
2023-09-04 15:30:22 +0200

    feat: changed colors

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 const input = document.querySelector('.todo-name');
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+if (todoList.length > 0) {
+    renderTodoList();
+}
+
+function todoUpdate() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
 
 function renderTodoList() {
 
@@ -8,21 +16,22 @@ function renderTodoList() {
     todoList.forEach((todo, i) => {
         const {
             name,
-            dueDate
+            dueDate,
+            status
         } = todo;
         const html = `
-        <div class="todo-item" id="todo-name"> ${name}</div>
+        <div class="todo-status-name">
+            <button class="todo-status inputs ${status}"> 
+            </button> 
+            <div class="todo-item" id="todo-name"> ${name}</div>
+        </div>
+
         <div class="todo-item" id="todo-date"> ${dueDate}</div>
-        <div class="todo-status-delete">
-  
-        <button class="todo-status inputs"> 
-        </button> 
         
         <button class="todo-del inputs"> 
             <img class="todo-trash-icon" src="icons/trash.png">
         </button> 
-
-        </div>
+        
     `;
         todoListHTML += html;
     });
@@ -32,14 +41,45 @@ function renderTodoList() {
     document.querySelectorAll('.todo-del').forEach((button, i) => {
         button.addEventListener('click', () => {
             todoList.splice(i, 1);
+            todoUpdate();
             renderTodoList();
         });
     });
-    
+
+    document.querySelectorAll('.todo-status').forEach((button, i) => { 
+        let clickCount = 1;
+        button.addEventListener('click', () => {
+            clickCount++;
+            switch (clickCount%3) {
+                case 1:
+                    button.classList.add('todo-status-not-started');
+                    button.classList.remove('todo-status-completed');
+                    todoUpdate();
+                    break;
+                case 2:
+                    button.classList.add('todo-status-in-progress');
+                    button.classList.remove('todo-status-not-started');
+                    todoList[i].status = 'todo-status-in-progress'; 
+                    todoUpdate();
+                    break;
+                case 0:
+                    button.classList.add('todo-status-completed');
+                    button.classList.remove('todo-status-in-progress');
+                    todoList[i].status = 'todo-status-completed'; 
+                    todoUpdate();
+                    break;
+            }
+        });
+    });
 }
-
-
+ 
 document.querySelector('.todo-add').addEventListener('click', addTodo);
+document.querySelector('.todo-name').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      addTodo();
+    }
+  });
+  
 
 function addTodo() {
 
@@ -53,11 +93,13 @@ function addTodo() {
     const dateInput = document.querySelector('.todo-date');
     const dueDate = dateInput.value;
 
-
     todoList.push({
         name,
-        dueDate
+        dueDate,
+        status: 'todo-status-not-started'
     });
+
+    todoUpdate();
 
     input.value = '';
     renderTodoList();
