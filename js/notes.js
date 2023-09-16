71faa216f395126322b5eb4c26021a59eea5b233
azjat <90611765+azjat@users.noreply.github.com>
2023-09-16 12:29:07 +0200

    feat: button highlight now changes colors depending on choose color in colorPicker

--- js/notes.js
+++ js/notes.js
@@ -3,24 +3,51 @@ let advancedOptions = document.querySelectorAll(".advanced-options");
 let formatButtons = document.querySelectorAll(".format");
 let alignButtons = document.querySelectorAll(".align");
 let icons = document.querySelectorAll(".format-icon");
+let foreColor = document.querySelector("#foreColor");
+let colorHex;
+let colorRgba;
+
+foreColor.value = "#000000";
+
+function hexToRgba(hex) {
+    if (hex.charAt(0) === '#') {
+        hex = hex.slice(1);
+    }
+    if (hex.length !== 6) {
+        return null;
+    }
+    let r = parseInt(hex.slice(0, 2), 16);
+    let g = parseInt(hex.slice(2, 4), 16);
+    let b = parseInt(hex.slice(4, 6), 16);
+    return `rgba(${r},${g},${b},0.3)`;
+}
+
+function setColor(){
+    colorHex = foreColor.value;
+    colorRgba = hexToRgba(colorHex);
+    document.documentElement.style.setProperty('--rgba-color', colorRgba);
+};
+
+foreColor.addEventListener("change", setColor);
+
+setColor();
+
+
+
 
 const highlighter = (className, needsRemoval) => {
     className.forEach((button) => {
         button.addEventListener("click", () => {
-            //needsRemoval = true means only one button should be highlight and other would be normal
+            //needsRemoval = true means only one button should be highlighted and other would be normal
             if (needsRemoval) {
                 let alreadyActive = false;
-                console.log(button.classList);
                 //If currently clicked button is already active
                 if (button.classList.contains("active")) {
                     alreadyActive = true;
-                    console.log("already active");
                 }
                 //Remove highlight from other buttons
                 highlighterRemover(className);
                 if (!alreadyActive) {
-                    //highlight clicked button
-                    console.log("not active, added class");
                     button.classList.add("active");
                 }
             } else {
@@ -36,7 +63,6 @@ const highlighter = (className, needsRemoval) => {
 const highlighterRemover = (className) => {
     className.forEach((button) => {
         button.classList.remove("active");
-        console.log("removed the active class");
     });
 };
 
@@ -56,7 +82,6 @@ const modifyText = (command, defaultUi, value) => {
 optionsButtons.forEach((button) => {
     button.addEventListener("click", () => {
         modifyText(button.id, false, null);
-        console.log(button.id);
     });
 });
 
